b28b0b186945b8fc37f45a20ee6b02ea8e56d4c2
Deekshitha_Y <deekshithakmit@gmail.com>
2025-08-01 18:39:50 +0530

    adding happy number code

---
+++ happy-num.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ class Solution {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    //System.out.println("Enter a number");
+    int num = in.nextInt();
+     if(isHappy(num))
+      System.out.println("true");
+    else
+      System.out.println("false");
+  }
+
+  private static boolean isHappy(int num){
+    if(num == 1)
+      return true;
+    if(num == 4)
+      return false;
+     //recall the function with sum value
+    return isHappy(sumOfDigits(num));
+  }
+
+   //Function to return sum of square of digits
+  private static int sumOfDigits(int num){
+    int sum = 0;
+    while(num>0){
+      sum += Math.pow(num%10, 2);
+      num = num/10;
+    }
+    return sum;
+  }
+}
